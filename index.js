4c7e07d7bb8b0fed66ebde889411043299037078
RashikaBhardwaj <bhardwajrashika954@gmail.com>
2024-06-08 11:50:56 +0530

    javascript codes

---
+++ index.js
@@ -0,0 +1,85 @@
+// //var
+// var num=10;
+// {
+//     num=20;
+
+// }
+// console.log("var",num)
+
+// //let
+// let num1=10
+// {
+//     num1=20
+// }
+// console.log("let",num1)
+
+// //constant
+// const num2=10
+// {
+//     num2=20
+// }
+// console.log("cons",num2)
+
+// let num1=10;
+// let num2=20;
+
+// let sum = num1+num2;
+
+// console.log(sum)
+
+// let num1=10;
+// let num2=20;
+
+// let diff = num1-num2;
+
+// console.log(diff)
+
+// let num1=10;
+// let num2=20;
+
+// let mul = num1*num2;
+
+// console.log(mul)
+// let num1=10;
+// let num2=20;
+
+// let div = num1/num2;
+
+// console.log(div)
+
+// let num1=10;
+// let num2=20;
+
+// let mod = num1%num2;
+
+// console.log(mod)
+
+// let num1=30;
+// let num2=40;
+// let sum=num1+num2;
+// let dif=num1-num2;
+// let mul=num1*num2;
+// let mod=num1%num2;
+// let div=num1/num2;
+// console.log(sum)
+// console.log(dif)
+// console.log(mul)
+// console.log(mod)
+// console.log(div)
+
+// let num1=5;
+// let num2=10;
+// let sum=--num1+ ++num2 +num1-- +num2++
+// console.log(sum)
+// console.log(num1)
+// console.log(num2)
+
+// let a=10
+// let b=20
+// let c=30
+// let result=(a<b&&a<c)?"a":(b<c)?"b":"c"
+// console.log(result)
+
+let a=16
+let age=(a>18)?"can vote":"can not vote"
+console.log(age)
